f9c6d5feac569f255785b8d5b3b250a9d14c5723
SilentNN <akanam@gmail.com>
2020-12-08 22:18:42 -0800

    users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,45 @@
+class Api::UsersController < ApplicationControllerdef 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :index
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+    
+    def index
+        @users = User.all
+        render :index
+    end
+    
+    def update
+        @user = User.find_by(id: params[:id])
+        if @user.id == current_user.id && @user.update(user_params)
+            render :show
+        else
+            render json: ["Something went wrong!"], status: 401
+        end
+    end
+    
+    def destroy
+        @user = User.find_by(id: params[:id])
+        if @user.id == current_user.id && @user.destroy
+            render :show
+        else
+            render json: ["Something went wrong!"], status: 401
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+end
