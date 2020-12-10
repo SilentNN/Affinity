5e20e1641cf06e891580e6add8f8427babe96c01
SilentNN <akanam@gmail.com>
2020-12-10 00:44:12 -0800

    entities reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,11 +1,13 @@
 import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
 
 export default (
-    (state = {}, action) => {
+    (oldState = {}, action) => {
         const nextState = {...oldState};
         switch (action.type) {
             case RECEIVE_CURRENT_USER:
                 return {...nextState, [action.currentUser.id]: action.currentUser};
+            default:
+                return oldState;
         }
     }
 )
