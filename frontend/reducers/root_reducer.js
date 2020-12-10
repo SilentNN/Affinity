03461a3ae08fa68f01054ea04601ec4fec37e600
SilentNN <akanam@gmail.com>
2020-12-10 03:06:48 -0800

    root reducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux';
+
+import entities from './entities/entities_reducer';
+import errors from './errors/errors_reducer';
+import session from './session/session_reducer';
+
+export default combineReducers({
+    entities,
+    errors,
+    session,
+});
